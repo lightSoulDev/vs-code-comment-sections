65b3836574900a129137992c3f7dcbfd9d052586
Нуржан <100024122+lightSoulDev@users.noreply.github.com>
2022-02-28 04:51:04 +0600

    Project init

---
+++ src/comment.ts
@@ -0,0 +1,60 @@
+import { workspace } from "vscode";
+
+const comment = (selection: string): string => {
+  const userParams = workspace.getConfiguration("coolcommentsections.params");
+
+  let divider: string = userParams.get("divider") ?? "=-";
+  let blockLenght: number = userParams.get("blockLenght") ?? 50;
+  let adjust: boolean = userParams.get("adjust") ?? true;
+  let formatSelection: boolean = userParams.get("formatSelection") ?? true;
+  let singleLine: boolean = userParams.get("singleLine") ?? false;
+
+  let comment: string = "";
+
+  let maxWidth: number = blockLenght - (adjust ? (divider.length + 1) % 2 : 0);
+  let width: number = maxWidth;
+  let maxSelectionLenght: number = width - divider.length * 2 - 2;
+
+  let dividerLine: string = `${divider
+    .repeat(Math.ceil(width / divider.length))
+    .slice(0, width)}`;
+
+  let commentContents = [];
+
+  for (let selectionLine of selection.split("\n")) {
+    if (formatSelection)
+      selectionLine = selectionLine.toUpperCase().split("").join(" ");
+    selectionLine = selectionLine.slice(0, maxSelectionLenght);
+
+    let selectionLenght: number = selectionLine.length;
+    let rightSide: number = Math.ceil(
+      (width - selectionLenght - divider.length * 2) / 2
+    );
+    let leftSide: number =
+      width - rightSide - selectionLenght - divider.length * 2;
+
+    let contentFiller = singleLine ? divider : " ";
+
+    let content: string = `${divider}${
+      contentFiller
+        .repeat(Math.ceil(leftSide / contentFiller.length))
+        .slice(0, leftSide - 1) +
+      " " +
+      selectionLine +
+      " " +
+      contentFiller
+        .repeat(Math.ceil(rightSide / contentFiller.length))
+        .slice(0, rightSide - 1)
+    }${divider.split("").reverse().join("")}`;
+
+    commentContents.push(content);
+  }
+
+  if (singleLine) comment = commentContents.join("\n");
+  else
+    comment = `${dividerLine}\n${commentContents.join("\n")}\n${dividerLine}`;
+
+  return comment + "\n";
+};
+
+export default comment;
---
+++ src/extension.ts
@@ -0,0 +1,56 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import comment from "./comment";
+
+const generateComment = (editor: vscode.TextEditor) => {
+  const { document, selections } = editor;
+
+  editor.edit((b) => {
+    selections.forEach(async (selection) => {
+      let com: string = "";
+      if (!selection.isEmpty) {
+        const text = document.getText(selection);
+
+        com = comment(text);
+        b.replace(selection, com);
+      } else {
+        const { text, rangeIncludingLineBreak } = document.lineAt(
+          selection.active.line
+        );
+
+        com = comment(text);
+        b.replace(rangeIncludingLineBreak, com + "\n");
+      }
+    });
+    vscode.commands.executeCommand("editor.action.addCommentLine");
+  });
+};
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "coolcommentsections" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "coolcommentsections.generateComment",
+    async () => {
+      const { activeTextEditor: editor } = vscode.window;
+
+      // const symbol = await window.showInputBox({ placeHolder: "symbols" });
+      if (editor) generateComment(editor);
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
