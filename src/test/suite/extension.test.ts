a6c8d6fd1cd0d1c61afc19e5c86987f613aa55bc
Нуржан <100024122+lightSoulDev@users.noreply.github.com>
2022-02-28 15:17:43 +0600

    todo: add test cases

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,10 +6,11 @@ import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+    vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    // TODO: Add Tests
+    // test('Sample test', () => {
+    // 	assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    // 	assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    // });
 });
